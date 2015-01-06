542a0a5e1fe5229142b52d790444fb7310b72dcc
syntactical <greg.dutcher@gmail.com>
2015-01-06 15:24:09 -0500

    add login

--- app/views.py
+++ app/views.py
@@ -1,9 +1,31 @@
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
 	user = {'nickname': 'Paul Toadman'}
-	return render_template('index.html',
-							title='Home',
-							user=user)
+	listings = [
+		{
+			'title': "FAKE CHRISTMAS TREES",
+			'description': "50,000 fake Christmas trees. All must go by Friday. Local pickup only"
+		},
+		{
+			'title': "Rodeo Equipment",
+			'description': "Rodeo equipment, all must go. Local pickup only"
+		}
+	]
+	return render_template('index.html', title="Hello", user=user, listings=listings)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="%s", remember_me=%s' %
+			(form.openid.data, str(form.remember_me.data)))
+		return redirect('/index')
+	return render_template('login.html',
+							title="Sign In",
+							form=form,
+							providers=app.config['OPENID_PROVIDERS'])
